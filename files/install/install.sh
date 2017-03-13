9b697739f927f8cb3813b83260c22f5972c7a385
king-h <HolgerKing@gmx.net>
2017-03-13 11:55:36 +0100

    Updated package dependencies

--- files/install/install.sh
+++ files/install/install.sh
@@ -5,31 +5,42 @@ echo 'deb http://packages.debian.org jessie stable' >> /etc/apt/sources.list
 apt-get install -qy --force-yes --no-install-recommends wget \
                                                         unzip
 
-# Install window manager and x-server
+# Install "Openbox" (window manager) and a leightweight VNC-/X-server combination
+# called "vnc4server" (details see: http://www.butschek.de/fachartikel/vnc4server/).
 apt-get install -qy --force-yes --no-install-recommends vnc4server \
                                                         x11-xserver-utils \
-                                                        openbox \
 							xfonts-base \
 							xfonts-100dpi \
 							xfonts-75dpi \
+                                                        openbox \
+							openbox-themes \
+							obconf \
+							obmenu \
+							xterm \
+							firefox-esr \
 							libfuse2
 
-# Install xrdp
+# Install "xrdp"
 apt-get install -qy --force-yes --no-install-recommends xrdp
+
+# Install network tools like "telnet" and "net-tools" (including "netstat") for testing purposes
+apt-get install -qy --force-yes --no-install-recommends telnet net-tools
 
 # Install runit referenced and needed in "my_init" Python script
 apt-get install -qy --force-yes --no-install-recommends runit
 
-# Install Guac
+# Install "Guacamole" web client web application in combination with "Apache Tomcat"
+# and the "VNC support plugin for Guacamole"
 apt-get install -qy --force-yes --no-install-recommends openjdk-8-jdk \
 							libossp-uuid-dev \
                                                         libpng12-dev \
                                                         libfreerdp-dev \
                                                         libcairo2-dev \
                                                         tomcat8 \
-                                                        guacamole
+                                                        guacamole \
+							libguac-client-vnc0
 
-# Install less
+# Install "less" (for easily reading/tailing files)
 apt-get install -qy --force-yes --no-install-recommends less
 
 # Python Skripten bereitstellen
