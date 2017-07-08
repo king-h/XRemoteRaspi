35a56573435537569a650c035588fe8241e9cd8e
king-h <HolgerKing@gmx.net>
2017-07-08 09:45:29 +0200

    Update install.sh
    Integrated downloading of latest Guacamole package

--- files/install/install.sh
+++ files/install/install.sh
@@ -277,5 +277,12 @@ ln -s /etc/sv/guacd /etc/service/guacd
 # setting correct execution permission rights for above added custom runit scripts
 find /etc/sv -name "run" -type f -exec chmod 755 {} \; -print
 
+#########################################
+### INSTALLATION ###
+#########################################
+
+# Download latest Guacamole package
+wget --user-agent=Mozilla -H --max-redirect=10 -O /tmp/guacamole-0.9.12-incubating.war "http://mirror.23media.de/apache/incubator/guacamole/0.9.12-incubating/binary/guacamole-0.9.12-incubating.war"
+
 # Python Skripten bereitstellen
 # /sbin/setuser und /sbin/my_init (launched when the container is started)
