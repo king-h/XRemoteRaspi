c81ec855c9f484bfe93bcf8108295ae220113b62
king-h <HolgerKing@gmx.net>
2017-07-08 10:58:40 +0200

    Update install.sh
    Added user option for command "sudo"

--- files/install/install.sh
+++ files/install/install.sh
@@ -71,7 +71,7 @@ exec 2>&1
 WD=${WIDTH:-1280}
 HT=${HEIGHT:-720}
 
-exec /usr/bin/sudo nobody Xvnc4 :1 -geometry ${WD}x${HT} -depth 16 -rfbwait 30000 -SecurityTypes None -rfbport 5901 -bs -ac \
+exec /usr/bin/sudo -u nobody Xvnc4 :1 -geometry ${WD}x${HT} -depth 16 -rfbwait 30000 -SecurityTypes None -rfbport 5901 -bs -ac \
 				   -pn -fp /usr/share/fonts/X11/misc/,/usr/share/fonts/X11/75dpi/,/usr/share/fonts/X11/100dpi/ \
 				   -co /etc/X11/rgb -dpi 96
 EOT
@@ -216,7 +216,7 @@ cat <<'EOT' > /etc/sv/openbox/run
 #!/bin/bash
 exec 2>&1
 
-exec env DISPLAY=:1 HOME=/nobody /usr/bin/sudo nobody /usr/bin/openbox-session
+exec env DISPLAY=:1 HOME=/nobody /usr/bin/sudo -u nobody /usr/bin/openbox-session
 EOT
 
 cat <<'EOT' > /etc/sv/openbox/log/run
