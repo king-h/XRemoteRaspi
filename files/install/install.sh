b60606f3054f4ab82f01dd17e265ecd25b9d25e0
king-h <HolgerKing@gmx.net>
2017-07-09 11:54:47 +0200

    Update install.sh
    Aligned "xrdp.ini" to existing new version of xRDP configuration file

--- files/install/install.sh
+++ files/install/install.sh
@@ -134,38 +134,138 @@ ln -s /etc/sv/xrdp /etc/service/xrdp
 # Providing a custom "Xrdp" configuration file "xrdp.ini"
 cat <<'EOT' > /etc/xrdp/xrdp.ini
 [globals]
-bitmap_cache=yes
-bitmap_compression=yes
-port=3389
-allow_channels=true
-max_bpp=16
+; xrdp.ini file version number
+ini_version=1
+
+; fork a new process for each incoming connection
 fork=yes
-crypt_level=low
+; tcp port to listen
+port=3389
+; regulate if the listening socket use socket option tcp_nodelay
+; no buffering will be performed in the TCP stack
+tcp_nodelay=true
+; regulate if the listening socket use socket option keepalive
+; if the network connection disappear without close messages the connection will be closed
+tcp_keepalive=true
+
+; security layer can be 'tls', 'rdp' or 'negotiate'
+; for client compatible layer
 security_layer=rdp
-tcp_nodelay=yes
-tcp_keepalive=yes
+; minimum security level allowed for client
+; can be 'none', 'low', 'medium', 'high', 'fips'
+crypt_level=low
+; X.509 certificate and private key
+; openssl req -x509 -newkey rsa:2048 -nodes -keyout key.pem -out cert.pem -days 365
+certificate=
+key_file=
+; specify whether SSLv3 should be disabled
+#disableSSLv3=true
+; set TLS cipher suites
+#tls_ciphers=HIGH
+
+; Section name to use for automatic login if the client sends username
+; and password. If empty, the domain name sent by the client is used.
+; If empty and no domain name is given, the first suitable section in
+; this file will be used.
+autorun=xrdp1
+
+allow_channels=true
+allow_multimon=false
+bitmap_cache=true
+bitmap_compression=yes
+bulk_compression=true
+hidelogwindow=true
+max_bpp=16
+new_cursors=true
+; fastpath - can be 'input', 'output', 'both', 'none'
+use_fastpath=both
+
+;
+; colors used by windows in RGB format
+;
 blue=009cb5
 grey=dedede
-autorun=xrdp1
-bulk_compression=yes
-new_cursors=yes
-use_fastpath=both
-hidelogwindow=yes
+#black=000000
+#dark_grey=808080
+#blue=08246b
+#dark_blue=08246b
+#white=ffffff
+#red=ff0000
+#green=00ff00
+#background=626c72
 
-[xrdp1]
-name=GUI_APPLICATION
-lib=libvnc.so
-username=nobody
-password=PASSWD
-ip=127.0.0.1
-port=5901
+;
+; configure login screen
+;
+
+; Login Screen Window Title
+ls_title=LibreElec Container Remote Control
+
+; top level window background color in RGB format
+ls_top_window_bg_color=009cb5
+
+; width and height of login screen
+ls_width=350
+ls_height=430
+
+; login screen background color in RGB format
+ls_bg_color=dedede
+
+; optional background image filename (bmp format).
+#ls_background_image=
+
+; logo
+; full path to bmp-file or file in shared folder
+ls_logo_filename=
+ls_logo_x_pos=55
+ls_logo_y_pos=50
+
+; for positioning labels such as username, password etc
+ls_label_x_pos=30
+ls_label_width=60
+
+; for positioning text and combo boxes next to above labels
+ls_input_x_pos=110
+ls_input_width=210
+
+; y pos for first label and combo box
+ls_input_y_pos=220
+
+; OK button
+ls_btn_ok_x_pos=142
+ls_btn_ok_y_pos=370
+ls_btn_ok_width=85
+ls_btn_ok_height=30
+
+; Cancel button
+ls_btn_cancel_x_pos=237
+ls_btn_cancel_y_pos=370
+ls_btn_cancel_width=85
+ls_btn_cancel_height=30
 
 [channels]
+; Channel names not listed here will be blocked by XRDP.
+; You can block any channel by setting its value to false.
+; IMPORTANT! All channels are not supported in all use
+; cases even if you set all values to true.
+; You can override these settings on each session type
+; These settings are only used if allow_channels=true
 rdpdr=true
 rdpsnd=true
 drdynvc=true
 cliprdr=true
 rail=true
+xrdpvr=true
+tcutils=true
+
+[xrdp1]
+name=VNC Remote
+lib=libvnc.so
+username=ask
+password=ask
+ip=127.0.0.1
+port=5901
+
 EOT
 
 # Configuring "xrdp-sesman" service for the "runit" init daemon
